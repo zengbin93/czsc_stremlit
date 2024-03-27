39778f45a225f4de52fd5e9389cfb7fd1a5f6925
zengbin93 <zeng_bin8888@163.com>
2024-03-27 09:41:30 +0800

    update

--- CZSC.py
+++ CZSC.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 
 st.set_page_config(layout="wide")
-
+st.divider()
 st.write("代码库：https://github.com/waditu/czsc")
 st.write("数据源：https://tushare.pro/")
 st.write("作者：zengbin93")
